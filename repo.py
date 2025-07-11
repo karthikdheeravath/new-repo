6388000788fc577e81bc1da30506fe654fe2384f
karth <karhikdheeravath718@gmail.com>
2025-07-11 23:00:34 +0530

    prime updated

--- repo.py
+++ repo.py
@@ -1,2 +1,9 @@
-print("Today Repo")
-print("Branch Is Created in the branch")
+n = int(input("enter a number"))
+count =0
+for i in range(1, n+1):
+    if n%i==0:
+        count = count + 1
+if count ==2:
+    print("prime number")
+else:
+    print("not a prime number")
